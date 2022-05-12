fa3b9f6edab17edba6f902afe0618fcfb1625dae
Barmaja98 <105444151+Barmaja98@users.noreply.github.com>
2022-05-12 19:01:25 +0000

    Lambda refactoring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from random import random
 
-def montecarlo(n):
-  return sum((random() for _ in range(n)))/n
+montecarlo = lambda n : sum((random() for _ in range(n)))/n
 
 for _ in range(10):
   print(montecarlo(1000000))
