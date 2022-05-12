be264d4a0e9c9b34efdd001127963a776c9c5f95
Barmaja98 <105444151+Barmaja98@users.noreply.github.com>
2022-05-12 20:51:09 +0000

    Pi trial 1

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from random import random
 
 montecarlo = lambda n : sum((random() for _ in range(n)))/n
 
-for _ in range(10):
-  print(montecarlo(1000000))
+dist = int(random()^2 + random()^2 < 1)
+pi_est = lambda n : sum((dist for _ in range(n))/n
+
+
+# for _ in range(10):
+#   print(montecarlo(1000000))
+
+print(pi_est(100))
