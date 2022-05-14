0813940257bebf702564166de152d49d29dbf9f6
Barmaja98 <105444151+Barmaja98@users.noreply.github.com>
2022-05-14 23:10:40 +0000

    Parenthesis

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import random
 
 montecarlo = lambda n : sum((random() for _ in range(n)))/n
 
-dist = ((x := random())*x + (y := random())*y < 1) 
+dist = ((x := random())*x + (y := random())*y) < 1
 pi_est = lambda n : sum((dist for _ in range(n)))/n
 
 
