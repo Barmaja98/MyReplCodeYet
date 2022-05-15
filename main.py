3182344ffb876e26c6e601323dc32f286f56e015
Barmaja98 <105444151+Barmaja98@users.noreply.github.com>
2022-05-15 00:08:33 +0000

    Nats?

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from random import random
+from math import exp, abs
 
+nats = ￼lambda : abs(exp(random())) < 1
 dist = lambda : ((x := random())*x + (y := random())*y) < 1
 montecarlo = lambda fx, n : sum((fx() for _ in range(n)))/n
 
+def test(f):
+  for _ in range(10):
+    print(montecarlo(f, 1000000))
 
-for _ in range(10):
-  print(montecarlo(random, 1000000))
-
-for _ in range(10):
-  print(4 * montecarlo(dist, 1000000))
+test(random)
+test(dist)
