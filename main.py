48c62d4e8a07fa3329207f4e4da0ff71ea30be7a
Barmaja98 <105444151+Barmaja98@users.noreply.github.com>
2022-05-27 21:03:50 +0000

    Last Monte Carlo: general function f(x, y) < 0

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from random import random
-from math import exp, fabs
+from random import random, uniform, expovariate
+from math import exp
 
-nats = lambda : fabs(exp(random())) < 1
-pi = lambda : 4*(((x := random())*x + (y := random())*y) < 1)
-montecarlo = lambda fx, n : sum((fx() for _ in range(n)))/n
+par1 = lambda: (random() - (x := random()) * x) < 0
+par2 = lambda: random() - (x := random()) * x
+nats = lambda: exp(random()) + 1
+pi = lambda: 4 * (((x := random()) * x + (y := random()) * y) < 1)
+montecarlo = lambda fx, n: sum((fx() for _ in range(n))) / n
+
 
 def test(f):
-  for _ in range(10):
-    print(montecarlo(f, 1000000))
+    print(f.__name__, '\n')
+    for _ in range(4):
+        print(montecarlo(f, 100000))
 
-test(random)
-test(pi)
+
+# test(random)
+# test(pi)
+test(nats)
+test(par1)
+test(par2)
+'''
+for i in range(10):
+  test(lambda: expovariate(float(i+1)))
+'''
